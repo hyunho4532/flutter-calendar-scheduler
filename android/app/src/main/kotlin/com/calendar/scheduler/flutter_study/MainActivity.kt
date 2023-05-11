02ec0295aaf394cb959b78ce0c00645147258bf3
hyunho4562 <kyounguk192@gmail.com>
2023-05-11 22:25:30 +0900

    2023-05-11 프로젝트 세팅

---
+++ android/app/src/main/kotlin/com/calendar/scheduler/flutter_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.calendar.scheduler.flutter_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/screen/HomeScreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp (
+      home: HomeScreen(),
+    )
+  );
+}
